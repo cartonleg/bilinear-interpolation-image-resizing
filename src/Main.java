e24da0e0be9cc4896b0f8cd0fc8daa1e532cc6c3
moman <momaniammar95@gmail.com>
2025-08-02 23:33:22 +0300

    main file to run the class, and testing images and output of sizing down then back up

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            BufferedImage image = ImageIO.read(new File("images/gray-image.bmp"));
+            int height = image.getHeight();
+            int width = image.getWidth();
+            BilinearResizing.resizeBmpFile("images/gray-image.bmp", "images/gray-image-size-down.bmp", height/2, width/2);
+            BilinearResizing.resizeBmpFile("images/gray-image-size-down.bmp",  "images/gray-image-size-up.bmp", height*2, width*2);
+        }
+        catch (Exception e) {
+            System.err.println("Error resizing bmp file.");
+        }
     }
 }
